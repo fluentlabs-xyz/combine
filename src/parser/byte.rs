99974326e9975c6ee4bc42ae3475ab73cc40c9da
Stas Stepanov <stanislav@ankr.com>
2024-11-05 17:43:16 +0700

    feat[solana-program]: fixed partial-io version; fixed byte_parser match collission; expended gitignore

--- src/parser/byte.rs
+++ src/parser/byte.rs
@@ -37,7 +37,7 @@ impl_token_parser! { Digit(), u8, Expected<Satisfy<I, fn (u8) -> bool>> }
 macro_rules! byte_parser {
     ($name:ident, $ty:ident, $f:ident) => {{
         let f = static_fn! {
-            (c, u8) -> bool { AsciiChar::from(c).map(|c| c.$f()).unwrap_or(false) }
+            (c, u8) -> bool { AsciiChar::from(c).map(|var| var.$f()).unwrap_or(false) }
         };
         $ty(satisfy(f).expected(stringify!($name)), PhantomData)
     }};
